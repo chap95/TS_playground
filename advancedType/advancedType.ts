343c8ff865ccb62bde0701b238c12dd4cdc0c526
totobiyo <chap95@likelion.org>
2021-07-27 18:10:31 +0900

    add git ignore

--- advancedType/advancedType.ts
+++ advancedType/advancedType.ts
@@ -1,16 +1,19 @@
 // 1. conditional type (조건부 타입)
-declare function f<T extends boolean>(x: T): T extends true ? string : number;
-let x = f(Math.random() < 0.5);
+declare function funcTest<T extends boolean>(
+  x: T
+): T extends true ? string : number;
+let x = funcTest(Math.random() < 0.5);
 // x 의 type 은 string | number 가 된다.
-
+console.log(x);
 interface Foo {
   propA: boolean;
   propB: boolean;
 }
 
-declare function f<T>(x: T): T extends Foo ? string : number;
+declare function testing<T>(x: T): T extends Foo ? string : number;
 function foo<U>(x: U) {
-  let a = f(x); // a 의 타입은 string | number 인 union type을 가지게 된다.
+  let a = testing(x); // a 의 타입은 string | number 인 union type을 가지게 된다.
+
   let b: string | number = a; // a 가 b 에 할당이 가능한 이유는 a 의 타입이 string 또는 number 인 것은 확실하기 때문
 }
 
@@ -41,7 +44,7 @@ type NotNullableEmailAddress3 =
 type NotNullableEmailAddress4 = string | string[] | never | never;
 // 다음
 type NotNullableEmailAddress5 = string | string[];
-
+// ===================================================
 // 3. never
 // never는 일반적으로 함수의 return 타입으로 사용이된다.
 // 어떤 함수의 return 타입으로 never 로 정해질 경우, 해당 함수는 절대 return 을 하지 않거나 오류를 출력한다.
@@ -56,6 +59,24 @@ type testType<T> = T | never;
 //
 // NonNullable 타입을 정의할 때 null 과 undefined 가 extends 될 수 있으면 never 타입으로 지정해 두었댜.
 // 그렇기 때문에 NonNullable 타입에 null 이나 undefined 가 들어오게 되면 컴파일 에러가 일어나게 된다.
-
+// ===================================================
 // 4. infer
 // '추론하다' 라는 뜻을 가진 키워드 이다. 타입을 추론하는데 사용한다.
+// 타입을 추론한다는 건 단어만 봐도 알 수 있다. 실질적으로 어떤 기능을 하는지 알아보자
+// infer 는 conditional type 에 많이 사용한다.
+// 아래의 예시를 한 번 보자
+type UnpackArrayType<T> = T extends (infer R)[] ? R : T;
+type T1 = UnpackArrayType<number[]>;
+type T2 = UnpackArrayType<string>;
+// 이 예제 코드는 array type을 unpacking 해주는 타입을 conditional type 으로 만들어 놓은것이다.
+// 코드 설명을 잠깐 하자면
+// T1 은 number 타입이 되는데 number[] 가 (infer R)[] 의 부분집합이 될 수 있기 때문에 T1 은 number 가 된다.
+// 여기서 infer의 역할을 무엇인가?
+// 컴파일러에게 새로운 타입 변수 R 이 UnpackArrayType scope 안에 선언됐다고 알려주는 역할을 한다.
+// 아래 예제를 살펴보자
+type R = string;
+type R1<T> = T extends R ? R : number;
+type R2<T> = T extends infer R ? R : number;
+// R1 과 R2 의 차이점은 무엇일까
+// R1 에 사용된 R 은 위에서 선언한 R 타입을 참조한다.
+// 하지만 R2에 사용된 R은 R2 scope 에 선언되어서 type R 과 상관없는 타입 변수가 된다.
